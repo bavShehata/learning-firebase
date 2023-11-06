0e70272a3e81c24d6d2e80c68a9d5a10a618a611
bavshehata <bavly@bavlifyweb.com>
2023-11-06 18:22:10 +0200

    Sign up

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyAoQnHLMFSNLkCehUgqz1WGAoPqCGHfUXM",
   authDomain: "hello-world-1e5b3.firebaseapp.com",
@@ -96,4 +96,22 @@ updateForm.addEventListener("submit", (e) => {
   }).then(() => {
     updateForm.reset();
   });
+});
+
+// signing users up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("user created:", cred.user);
+      signupForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
