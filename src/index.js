2e7842627c6ed17637b77624019a28404c1653f5
bavshehata <bavly@bavlifyweb.com>
2023-11-06 17:15:22 +0200

    Setting up firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoQnHLMFSNLkCehUgqz1WGAoPqCGHfUXM",
+  authDomain: "hello-world-1e5b3.firebaseapp.com",
+  projectId: "hello-world-1e5b3",
+  storageBucket: "hello-world-1e5b3.appspot.com",
+  messagingSenderId: "341059410805",
+  appId: "1:341059410805:web:988135577c2ccde72c113c",
+};
+
+initializeApp(firebaseConfig);
