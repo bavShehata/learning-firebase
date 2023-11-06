6fc10e8d298197f1ba25300c6e889dc24c53a7f7
bavshehata <bavly@bavlifyweb.com>
2023-11-06 18:16:51 +0200

    Updaring documents

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {
   orderBy,
   serverTimestamp,
   getDoc,
+  updateDoc,
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -79,4 +80,18 @@ const docRef = doc(db, "books", "0icaVHaVJ94J53XsPrHZ");
 
 onSnapshot(docRef, (doc) => {
   console.log(doc.data(), doc.id);
+});
+
+// updating a document
+const updateForm = document.querySelector(".update");
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let docRef = doc(db, "books", updateForm.id.value);
+
+  updateDoc(docRef, {
+    title: "updated title",
+  }).then(() => {
+    updateForm.reset();
+  });
 });
